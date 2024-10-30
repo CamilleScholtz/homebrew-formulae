7f4aa3c62ad747734077b7c3bc1e1b9d832ac6c1
Camille Scholtz <admin@camille.sh>
2024-10-30 20:33:25 +0100

    Update mmspsp

--- Casks/mmpsp.rb
+++ Casks/mmpsp.rb
@@ -3,9 +3,9 @@ cask "mmpsp" do
   desc "MacOS music player swift popover"
   homepage "https://github.com/CamilleScholtz/mmpsp"
 
-  version "0.3"
+  version "0.4"
   url "https://github.com/CamilleScholtz/mmpsp/releases/download/v#{version}/mmpsp.app.zip"
-  sha256 "ac9d598585d51cd07d124c3035c68fb31a719ddeb1c869252fb9b2c3a1b69fc6"
+  sha256 "879ba2b8ddae5f4664d4058de2c5a20074acdea0e6f39e0ca94d307144986596"
 
   app "mmpsp.app"
 end
